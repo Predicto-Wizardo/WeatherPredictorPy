2696f71bc6cb54128e7554f90adddf4d5175918e
Anthony Luo <anthonyluo100@gmail.com>
2022-03-02 20:06:59 -0500

    Try to get response from openweather. Getting 401 unauthorized

--- annitest.py
+++ annitest.py
@@ -1,24 +1,5 @@
-# import the module
-import python_weather
-import asyncio
+import requests
 
-async def getweather():
-    # declare the client. format defaults to the metric system (celcius, km/h, etc.)
-    client = python_weather.Client(format=python_weather.METRIC)
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid= {0b668fb873a34ff03efcfea09de00305}")
 
-    # fetch a weather forecast from a city
-    weather = await client.find("Waterloo")
-
-    # returns the current day's forecast temperature (int)
-    print(weather.current.temperature)
-
-    # get the weather forecast for a few days
-    for forecast in weather.forecasts:
-        print(str(forecast.date), forecast.sky_text, forecast.temperature)
-
-    # close the wrapper once done
-    await client.close()
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(getweather())
+print(response)

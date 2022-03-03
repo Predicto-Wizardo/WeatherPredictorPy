15784dbf57af2d4160a4bb3bda8cf0246807ea19
Anthony Luo <anthonyluo100@gmail.com>
2022-03-02 20:19:04 -0500

    Update annitest.py

--- annitest.py
+++ annitest.py
@@ -1,5 +1,11 @@
 import requests
 
-response = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid= {0b668fb873a34ff03efcfea09de00305}")
+openweather_url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=0b668fb873a34ff03efcfea09de00305"
 
-print(response)
+response = requests.get(openweather_url)
+
+print(response.json())
+
+for elem in response.json():
+    print(elem)
+    print(response.json()[elem])

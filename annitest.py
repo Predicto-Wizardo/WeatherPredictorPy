9dbfc5fbc9321a166b3ced23410cbb73e66b7aaa
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2022-03-02 20:54:31 -0500

    created method of recording down 'main' data from the api

--- annitest.py
+++ annitest.py
@@ -1,11 +1,28 @@
 import requests
+import time
+import csv
+
 
 openweather_url = "https://api.openweathermap.org/data/2.5/weather?lat=43.47&lon=-80.54&units=metric&appid=0b668fb873a34ff03efcfea09de00305"
 
 response = requests.get(openweather_url)
 
-print(response.json())
+# print(response.json())
+#
+# for elem in response.json():
+#     print(elem)
+#     print(response.json()[elem])
 
-for elem in response.json():
-    print(elem)
-    print(response.json()[elem])
+f = open('testinput.csv', 'w', newline='')
+fieldnames = response.json()['main']
+writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+t = 10
+writer.writeheader()
+
+for i in range(6):
+    response = requests.get(openweather_url)
+    row = response.json()['main']
+
+    writer.writerow(row)
+    time.sleep(t)

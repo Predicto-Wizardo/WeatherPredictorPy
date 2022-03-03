7c247203018bb9722d81d4830ea6ace049131505
Elina Zheng <47897713+elinxaaa@users.noreply.github.com>
2022-03-02 20:23:39 -0500

    kelvin to celcius

--- annitest.py
+++ annitest.py
@@ -1,6 +1,6 @@
 import requests
 
-openweather_url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=0b668fb873a34ff03efcfea09de00305"
+openweather_url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=0b668fb873a34ff03efcfea09de00305"
 
 response = requests.get(openweather_url)
 

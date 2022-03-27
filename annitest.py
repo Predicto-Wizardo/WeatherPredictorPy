d9ee5882934cc88d44fc2009443f7592bce14ca9
Anthony Luo <anthonyluo100@Gmail.com>
2022-03-27 19:51:50 -0400

    stripping dictionary to get first element (temp)
    Saves element 'temp' from our dict to our array.

--- annitest.py
+++ annitest.py
@@ -1,7 +1,7 @@
 import requests
 import time
 import csv
-
+import numpy
 
 openweather_url = "https://api.openweathermap.org/data/2.5/weather?lat=43.47&lon=-80.54&units=metric&appid=0b668fb873a34ff03efcfea09de00305"
 
@@ -19,11 +19,13 @@ writer = csv.DictWriter(f, fieldnames=fieldnames)
 
 t = 10 # set x amount of seconds to wait
 writer.writeheader()
+myarr = []
 
 for i in range(6):
     response = requests.get(openweather_url)
     row = response.json()['main']
-
+    print(row['temp'])
+    myarr.append(row['temp'])
     writer.writerow(row)
     time.sleep(t)
 
